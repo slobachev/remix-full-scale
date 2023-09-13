4735c910cdee79f101e59781bfe6fcd04743b30b
slobachev <sergeylobachev@outlook.com>
2023-09-13 17:23:43 +0300

    add css, font

---
+++ app/routes/loglines.$id.tsx
@@ -0,0 +1,11 @@
+export default function LoglineRoute() {
+  return (
+    <div>
+      <p>Here's your breathtaking logline:</p>
+      <p>
+        Why don't you find hippopotamuses hiding in trees?
+        They're really good at it.
+      </p>
+    </div>
+  );
+}
