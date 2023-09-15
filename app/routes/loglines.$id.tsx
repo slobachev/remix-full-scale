c7e3b957bf5a02e243829dead1fc9a5e36e36eed
slobachev <sergeylobachev@outlook.com>
2023-09-15 14:27:26 +0300

    add some routes

--- app/routes/loglines.$id.tsx
+++ app/routes/loglines.$id.tsx
@@ -1,11 +1,27 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const logline = await db.logline.findUnique({
+    where: { id: params.id },
+  });
+  if (!logline) {
+    throw new Error("Logline not found");
+  }
+  return json({ logline });
+};
+
 export default function LoglineRoute() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div>
-      <p>Here's your breathtaking logline:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees?
-        They're really good at it.
-      </p>
+      <p>Here's your fantastic logline:</p>
+      <p>{data.logline.content}</p>
+      <Link to=".">"{data.logline.name}" Permalink</Link>
     </div>
   );
 }
--- app/routes/loglines.tsx
+++ app/routes/loglines.tsx
@@ -1,11 +1,67 @@
-import { Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Link,
+  Outlet,
+  useLoaderData,
+} from "@remix-run/react";
 
-export default function JokesRoute() {
+import stylesUrl from "~/styles/loglines.css";
+import { db } from "~/utils/db.server";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
+
+export const loader = async () => {
+  return json({
+    loglineListItems: await db.logline.findMany({
+      orderBy: { createdAt: "desc" },
+      select: { id: true, name: true },
+      take: 5,
+     }),
+  });
+};
+
+export default function LoglinesRoute() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div>
-      <h1>MOVIE LOGLINES</h1>
-      <main>
-        <Outlet />
+    <div className="loglines-layout">
+      <header className="loglines-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link
+              to="/"
+              title="Remix Loglines"
+              aria-label="Remix Loglines"
+            >
+              <span className="logo">🤪</span>
+              <span className="logo-medium">M🤪VIE LOGLINES</span>
+            </Link>
+          </h1>
+        </div>
+      </header>
+      <main className="loglines-main">
+        <div className="container">
+          <div className="loglines-list">
+            <Link to=".">Get a random logline</Link>
+            <p>Here are a few more loglines to check out:</p>
+            <ul>
+              {data.loglineListItems.map(({ id, name }) => (
+                <li key={id}>
+                  <Link to={id}>{name}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="loglines-outlet">
+            <Outlet />
+          </div>
+        </div>
       </main>
     </div>
   );
