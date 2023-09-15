542f2c4ec039fb52ee0c82e20ba758062d27b43f
slobachev <sergeylobachev@outlook.com>
2023-09-15 14:38:10 +0300

    add a screen with randomizer

--- app/routes/loglines._index.tsx
+++ app/routes/loglines._index.tsx
@@ -1,11 +1,28 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  const count = await db.logline.count();
+  const index = Math.floor(Math.random() * count);
+  const [logline] = await db.logline.findMany({
+    skip: index,
+    take: 1
+  })
+  return json({ logline });
+};
+
 export default function LoglinesIndexRoute() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <p>Here's a random logline:</p>
       <p>
-        I was wondering why the frisbee was getting bigger,
-        then it hit me.
+        {data.logline.content}
       </p>
+      <Link to={data.logline.id}>
+        "{data.logline.name}" Permalink
+      </Link>
     </div>
   );
 }
