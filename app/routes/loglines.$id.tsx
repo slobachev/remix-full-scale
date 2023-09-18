d6ac70e5bc0fded00c5c3ab3948b65f60c0a183d
slobachev <sergeylobachev@outlook.com>
2023-09-18 23:47:33 +0300

    add delete

--- app/routes/loglines.$id.tsx
+++ app/routes/loglines.$id.tsx
@@ -1,10 +1,12 @@
-import type { LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { Link, isRouteErrorResponse, useLoaderData, useParams, useRouteError } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
+import { getUserId, requireUserId } from "~/utils/session.server";
 
-export const loader = async ({ params }: LoaderArgs) => {
+export const loader = async ({ request, params }: LoaderArgs) => {
+  const userId = await getUserId(request);
   const logline = await db.logline.findUnique({
     where: { id: params.id },
   });
@@ -13,17 +15,57 @@ export const loader = async ({ params }: LoaderArgs) => {
       status: 404,
     });
   }
-  return json({ logline });
+  return json({ isOwner: userId === logline.authorId, logline });
+};
+
+export const action = async ({
+  params,
+  request,
+}: ActionArgs) => {
+  const form = await request.formData();
+  if (form.get("_action") !== "delete") {
+    throw new Response(
+      `The action ${form.get("_action")} is not supported`,
+      { status: 400 }
+    );
+  }
+  const userId = await requireUserId(request);
+  const logline = await db.logline.findUnique({
+    where: { id: params.id },
+  });
+  if (!logline) {
+    throw new Response("Can't delete what does not exist", {
+      status: 404,
+    });
+  }
+  if (logline.authorId !== userId) {
+    throw new Response(
+      "Pssh, nice try. That's not your logline",
+      { status: 403 }
+    );
+  }
+  await db.logline.delete({ where: { id: params.id } });
+  return redirect("/loglines");
 };
 
 export default function LoglineRoute() {
-  const data = useLoaderData<typeof loader>();
+  const { logline, isOwner } = useLoaderData<typeof loader>();
 
   return (
     <div>
       <p>Here's your fantastic logline:</p>
-      <p>{data.logline.content}</p>
-      <Link to=".">"{data.logline.name}" Permalink</Link>
+      <p>{logline.content}</p>
+      <Link to=".">"{logline.name}" Permalink</Link>
+      {isOwner && <form method="post">
+        <button
+          className="button"
+          name="_action"
+          type="submit"
+          value="delete"
+        >
+          Delete
+        </button>
+      </form>}
     </div>
   );
 }
