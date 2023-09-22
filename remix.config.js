facdb6b27bab780492da104f4cb1ccb312424b83
slobachev <sergeylobachev@outlook.com>
2023-09-22 12:25:48 +0300

    add netlify

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,22 @@
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  ...baseConfig,
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverModuleFormat: "cjs",
+  // See https://remix.run/docs/en/main/file-conventions/route-files-v2
+  future: {
+    v2_routeConvention: true,
+  }
+  // add your own custom config here if you want to.
+  //
+  // See https://remix.run/docs/en/v1/file-conventions/remix-config
 };
